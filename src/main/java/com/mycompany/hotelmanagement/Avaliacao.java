e11f7b972a9e974a05a24b2dfb4ba5650c279706
GustavoOliveira2000 <165520705+GustavoOliveira2000@users.noreply.github.com>
2025-02-06 17:22:32 +0000

    Implementação da classe Avaliacao #22

--- src/main/java/com/mycompany/hotelmanagement/Avaliacao.java
+++ src/main/java/com/mycompany/hotelmanagement/Avaliacao.java
@@ -1,5 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+/**
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.hotelmanagement;
@@ -11,12 +11,11 @@ package com.mycompany.hotelmanagement;
 
 public class Avaliacao 
 {
+
     private int id;
-    private int reservaId;
+    private string Hospedes ;
+    private int Reserva;
     private int nota; // Exemplo: de 1 a 5
     private String comentario;
 
-    
-
-    
 }
